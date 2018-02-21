f15a0c89c35a78e221540b1668e6898c7247aace
Ivan Towlson <itowlson@microsoft.com>
2018-02-21 16:27:44 +1300

    Very interim transfer commit

--- src/components/clusterprovider/azure/azureclusterprovider.ts
+++ src/components/clusterprovider/azure/azureclusterprovider.ts
@@ -37,29 +37,51 @@ async function handleCreate(request: restify.Request, response: restify.Response
 
 async function getHandleCreateHtml(request: restify.Request): Promise<string> {
     const id: string = request.query["id"];
+    const subscription: string = request.query["subscription"];
 
-    const slr = await azure.getSubscriptionList(context, id);
-    if (!slr.result.succeeded) {
-        return `<h1>Error ${slr.actionDescription}</h1><p>Error details: ${slr.result.error[0]}</p>`;
+    if (id && !subscription) {
+
+        const mklink = (sub: string) => encodeURI(`http://localhost:${clusterPort}/create?id=${id}&subscription=${sub}`);
+
+        const slr = await azure.getSubscriptionList(context, id);
+        if (!slr.result.succeeded) {
+            return `<h1>Error ${slr.actionDescription}</h1><p>Error details: ${slr.result.error[0]}</p>`;
+        }
+
+        const subscriptions : string[] = slr.result.result;
+
+        if (!subscriptions || !subscriptions.length) {
+            return `<h1>You have no Azure subscriptions</h1>`;
+        }
+
+        const initialUri = mklink(subscriptions[0]);
+        const options = subscriptions.map((s) => `<option value="${mklink(s)}">${s}</option>`).join('\n');
+        return `<!-- PromptForSubscription -->
+                <html><body>
+                <style id='styleholder'>
+                </style>
+                <script>
+                function selectionChanged() {
+                    var selectCtrl = document.getElementById('selector');
+                    var selection = selectCtrl.options[selectCtrl.selectedIndex].value;
+                    document.getElementById('nextlink').href = selection;
+                }
+                </script>
+                <h1 id='h'>Choose subscription</h1>
+                <div id='content'>
+                <p>
+                Azure subscription: <select id='selector' onchange='selectionChanged()'>
+                ${options}
+                </select>
+                </p>
+
+                <p><b>Important! The selected subscription will be set as the active subscription for the Azure CLI.</b></p>
+
+                <p>
+                <a id='nextlink' href='${initialUri}'>Next &gt;</a>
+                </p>
+                </body></html>`;
+    } else if (id && subscription) {
+        return `<h1>You chose ${subscription}</h1>`;
     }
-
-    const subscriptions : string[] = slr.result.result;
-    const options = subscriptions.map((s) => `<option value="${s}">${s}</option>`).join('\n');
-    return `<!-- PromptForSubscription -->
-            <html><body>
-            <style id='styleholder'>
-            </style>
-            <h1 id='h'>Choose subscription</h1>
-            <div id='content'>
-            <p>
-            Azure subscription: <select id='selector' onchange='selectionChanged()'>
-            ${options}
-            </select>
-            </p>
-
-            <p><b>Important! The selected subscription will be set as the active subscription for the Azure CLI.</b></p>
-
-            <p>
-            </p>
-            </body></html>`;
 }
