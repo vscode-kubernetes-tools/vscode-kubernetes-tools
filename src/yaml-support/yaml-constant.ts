519399a831a7abb527ec8def1f3d33cae53a2788
Andy Xu(devdiv) <andxu@microsoft.com>
2018-04-03 10:09:17 +0800

    add enums for properties(kind, apiVersion is not included) (#152)

--- src/yaml-support/yaml-constant.ts
+++ src/yaml-support/yaml-constant.ts
@@ -10,6 +10,8 @@ export const KUBERNETES_SCHEMA_VERSION = '1.9.3';
 
 export const KUBERNETES_SCHEMA_FILE = path.join(__dirname, `../../../schema/swagger-v${KUBERNETES_SCHEMA_VERSION}.json`);
 
+export const KUBERNETES_SCHEMA_ENUM_FILE = path.join(__dirname, `../../../schema/schema_enums-v${KUBERNETES_SCHEMA_VERSION}.json`);
+
 export const KUBERNETES_GROUP_VERSION_KIND =  'x-kubernetes-group-version-kind';
 
 export const GROUP_VERSION_KIND_SEPARATOR =  '@';
