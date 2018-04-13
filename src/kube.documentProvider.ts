12d92a70866c046963908b96ad3859c975922a4a
andxu <andxu@microsoft.com>
2018-04-13 10:44:35 +0800

    make method provideTextDocumentContent async
    Signed-off-by: andxu <andxu@microsoft.com>

--- src/kube.documentProvider.ts
+++ src/kube.documentProvider.ts
@@ -7,29 +7,26 @@ export class KubernetesDocumentProvider implements vscode.TextDocumentContentPro
     public constructor(private kubectl: Kubectl) {
 
     }
-    provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
-        return new Promise<string>(async (resolve, reject) => {
-            if (uri.authority !== 'loadkubernetescore') {
-                vscode.window.showErrorMessage('Unrecognized operation: ' + uri.authority);
-                resolve('Unrecognized operation: ' + uri.authority);
-                return;
-            }
-            const value = querystring.parse(uri.query).value;
-            if (!value) {
-                vscode.window.showErrorMessage(`Missing required value in uri: ${uri}`);
-                resolve(`Missing required value in uri: ${uri}`);
-                return;
-            }
+
+    async provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): Promise<string> {
+        if (uri.authority !== 'loadkubernetescore') {
+            vscode.window.showErrorMessage('Unrecognized operation: ' + uri.authority);
+            return `Unrecognized operation: ${uri.authority}`;
+        }
+        const value = querystring.parse(uri.query).value;
+        if (!value) {
+            vscode.window.showErrorMessage(`Missing required value in uri: ${uri}`);
+            return `Missing required value in uri: ${uri}`;
+        }
 
 
-            const shellResult = await this.kubectl.invokeAsyncWithProgress(" -o json get " + value, `Loading ${value}...`);
+        const shellResult = await this.kubectl.invokeAsyncWithProgress(" -o json get " + value, `Loading ${value}...`);
 
-            if (shellResult.code !== 0) {
-                vscode.window.showErrorMessage('Get command failed: ' + shellResult.stderr);
-                resolve(shellResult.stderr);
-            } else {
-                resolve(shellResult.stdout);
-            }
-        });
+        if (shellResult.code !== 0) {
+            vscode.window.showErrorMessage('Get command failed: ' + shellResult.stderr);
+            return shellResult.stderr;
+        } else {
+            return shellResult.stdout;
+        }
     }
 }
