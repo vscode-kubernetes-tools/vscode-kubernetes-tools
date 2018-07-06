ed72e868bee87ca28635d46131f2a6ae65dbfc89
itowlson <ivan.towlson@microsoft.com>
2018-07-06 14:10:21 +1200

    Basic chart listing under repo

--- src/helm.repoExplorer.ts
+++ src/helm.repoExplorer.ts
@@ -1,5 +1,7 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
+import * as download from 'download';
+import * as YAML from 'yamljs';
 
 import { Host } from './host';
 import * as helm from './helm.exec';
@@ -9,14 +11,14 @@ export function create(host: Host): HelmRepoExplorer {
     return new HelmRepoExplorer(host);
 }
 
-interface HelmRepo {
-    readonly name: string;
-    readonly url: string;
+interface HelmObject {
+    getChildren(): Promise<HelmObject[]>;
+    getTreeItem(): vscode.TreeItem;
 }
 
-export class HelmRepoExplorer implements vscode.TreeDataProvider<HelmRepo> {
-    private onDidChangeTreeDataEmitter: vscode.EventEmitter<HelmRepo | undefined> = new vscode.EventEmitter<HelmRepo | undefined>();
-    readonly onDidChangeTreeData: vscode.Event<HelmRepo | undefined> = this.onDidChangeTreeDataEmitter.event;
+export class HelmRepoExplorer implements vscode.TreeDataProvider<HelmObject> {
+    private onDidChangeTreeDataEmitter: vscode.EventEmitter<HelmObject | undefined> = new vscode.EventEmitter<HelmObject | undefined>();
+    readonly onDidChangeTreeData: vscode.Event<HelmObject | undefined> = this.onDidChangeTreeDataEmitter.event;
 
     constructor(private readonly host: Host) {
         host.onDidChangeConfiguration((change) => {
@@ -26,8 +28,54 @@ export class HelmRepoExplorer implements vscode.TreeDataProvider<HelmRepo> {
         });
     }
 
-    getTreeItem(element: HelmRepo): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        const treeItem = new vscode.TreeItem(element.name);
+    getTreeItem(element: HelmObject): vscode.TreeItem | Thenable<vscode.TreeItem> {
+        return element.getTreeItem();
+        // const treeItem = new vscode.TreeItem(element.name);
+        // treeItem.iconPath = {
+        //     light: vscode.Uri.file(path.join(__dirname, "../../images/light/helm-blue-vector.svg")),
+        //     dark: vscode.Uri.file(path.join(__dirname, "../../images/dark/helm-white-vector.svg")),
+        // };
+        // return treeItem;
+    }
+
+    getChildren(parent?: HelmObject): vscode.ProviderResult<HelmObject[]> {
+        if (parent) {
+            return parent.getChildren();
+        }
+
+        return this.getHelmRepos();
+    }
+
+    private async getHelmRepos(): Promise<HelmObject[]> {
+        const repos = await listHelmRepos();
+        if (failed(repos)) {
+            return [ new HelmError('Unable to list Helm repos') ];
+        }
+        return repos.result;
+    }
+
+    refresh(): void {
+        this.onDidChangeTreeDataEmitter.fire();
+    }
+}
+
+class HelmError implements HelmObject {
+    constructor(private readonly text: string) {}
+
+    getTreeItem(): vscode.TreeItem {
+        return new vscode.TreeItem(this.text);
+    }
+
+    async getChildren(): Promise<HelmObject[]> {
+        return [];
+    }
+}
+
+class HelmRepo implements HelmObject {
+    constructor(private readonly name: string, private readonly url: string) {}
+
+    getTreeItem(): vscode.TreeItem {
+        const treeItem = new vscode.TreeItem(this.name, vscode.TreeItemCollapsibleState.Collapsed);
         treeItem.iconPath = {
             light: vscode.Uri.file(path.join(__dirname, "../../images/light/helm-blue-vector.svg")),
             dark: vscode.Uri.file(path.join(__dirname, "../../images/dark/helm-white-vector.svg")),
@@ -35,24 +83,24 @@ export class HelmRepoExplorer implements vscode.TreeDataProvider<HelmRepo> {
         return treeItem;
     }
 
-    getChildren(parent?: any): vscode.ProviderResult<HelmRepo[]> {
-        if (parent) {
-            return [];
+    async getChildren(): Promise<HelmObject[]> {
+        const charts = await listHelmRepoCharts(this.url);
+        if (failed(charts)) {
+            return [ new HelmError('Error fetching charts') ];
         }
+        return charts.result;
+    }
+}
 
-        return this.getHelmRepos();
+class HelmRepoChart implements HelmObject {
+    constructor(private readonly name: string) {}
+
+    getTreeItem(): vscode.TreeItem {
+        return new vscode.TreeItem(this.name);
     }
 
-    private async getHelmRepos(): Promise<HelmRepo[]> {
-        const repos = await listHelmRepos();
-        if (failed(repos)) {
-            return [ { name: 'Unable to list Helm repos', url: '' } ];
-        }
-        return repos.result;
-    }
-
-    refresh(): void {
-        this.onDidChangeTreeDataEmitter.fire();
+    async getChildren(): Promise<HelmObject[]> {
+        return [];
     }
 }
 
@@ -67,6 +115,20 @@ async function listHelmRepos(): Promise<Errorable<HelmRepo[]>> {
                            .map((l) => l.trim())
                            .filter((l) => l.length > 0)
                            .map((l) => l.split('\t').map((bit) => bit.trim()))
-                           .map((bits) => ({ name: bits[0], url: bits[1] }));
+                           .map((bits) => new HelmRepo(bits[0], bits[1]));
     return { succeeded: true, result: repos };
+}
+
+async function listHelmRepoCharts(repoUrl: string): Promise<Errorable<HelmRepoChart[]>> {
+    const indexUrl = vscode.Uri.parse(repoUrl).with({ path: 'index.yaml' }).toString();
+    try {
+        const buffer: Buffer = await download(indexUrl);
+        const yaml = buffer.toString();
+        const chartData = YAML.parse(yaml);
+        const entries = chartData.entries;
+        const charts = Object.keys(entries).map((k) => new HelmRepoChart(k));
+        return { succeeded: true, result: charts };
+    } catch (e) {
+        return { succeeded: false, error: [ "" + e ] };
+    }
 }
