18f0c4abe910324414f32dffff275c96e2fa6ab3
Ivan Towlson <itowlson@microsoft.com>
2018-07-13 16:40:13 +1200

    Lazy load download module

--- src/components/download/download.ts
+++ src/components/download/download.ts
@@ -1,24 +1,27 @@
-const home = process.env['HOME'];
-
-import * as download_core from 'download';  // NEVER do a naked import of this as it corrupts $HOME on Windows - use wrapper functions from this module (or export the download function from this module if you really need the flexibility of the core implementation)
 import * as path from 'path';
 import * as stream from 'stream';
 import * as tmp from 'tmp';
 
 import { succeeded, Errorable } from '../../errorable';
 
-// Fix download module corrupting HOME environment variable on Windows
-// See https://github.com/Azure/vscode-kubernetes-tools/pull/302#issuecomment-404678781
-// and https://github.com/kevva/npm-conf/issues/13
-if (home) {
-    process.env['HOME'] = home;
-}
-
 type DownloadFunc =
     (url: string, destination?: string, options?: any)
          => Promise<Buffer> & stream.Duplex; // Stream has additional events - see https://www.npmjs.com/package/download
 
-const download: DownloadFunc = download_core;
+let download: DownloadFunc;
+
+function ensureDownloadFunc() {
+    if (!download) {
+        // Fix download module corrupting HOME environment variable on Windows
+        // See https://github.com/Azure/vscode-kubernetes-tools/pull/302#issuecomment-404678781
+        // and https://github.com/kevva/npm-conf/issues/13
+        const home = process.env['HOME'];
+        download = require('download');
+        if (home) {
+            process.env['HOME'] = home;
+        }
+    }
+}
 
 export async function toTempFile(sourceUrl: string): Promise<Errorable<string>> {
     const tempFileObj = tmp.fileSync({ prefix: "vsk-autoinstall-" });
@@ -30,6 +33,7 @@ export async function toTempFile(sourceUrl: string): Promise<Errorable<string>> 
 }
 
 export async function to(sourceUrl: string, destinationFile: string): Promise<Errorable<void>> {
+    ensureDownloadFunc();
     try {
         await download(sourceUrl, path.dirname(destinationFile), { filename: path.basename(destinationFile) });
         return { succeeded: true, result: null };
