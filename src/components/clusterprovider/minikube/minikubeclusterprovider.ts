2bef178cee379172e19be5ab9aae41bfe04c499b
Brendan Burns <bburns@microsoft.com>
2018-08-07 19:27:04 -0700

    Add an option for VM driver as well as additional flags. (#330)

--- src/components/clusterprovider/minikube/minikubeclusterprovider.ts
+++ src/components/clusterprovider/minikube/minikubeclusterprovider.ts
@@ -7,7 +7,7 @@ import { propagationFields, formPage } from '../common/form';
 import { refreshExplorer } from '../common/explorer';
 import { succeeded, Failed, failed } from '../../../errorable';
 import { Shell } from '../../../shell';
-import { Minikube } from './minikube';
+import { Minikube, MinikubeOptions } from './minikube';
 
 export interface Context {
     readonly shell: Shell;
@@ -130,7 +130,25 @@ async function promptForConfiguration(previousData: any, context: Context, actio
         submitText: 'Start Minikube',
         previousData: previousData,
         formContent: `
-        <p>No configuration options (yet)</p>
+        <table style='width:50%'>
+        <tr>
+        <td>Minikube VM Driver</td>
+        <td style='text-align: right'><select name='vmdriver' id='vmdriver'>
+           <option selected='true'>virtualbox</option>
+           <option>vmwarefusion</option>
+           <option>kvm</option>
+           <option>xhyve</option>
+           <option>hyperv</option>
+           <option>hyperkit</option>
+           <option>kvm2</option>
+           <option>none</option>
+        </select></td>
+        </tr>
+        <tr>
+        <td>Additional Flags:</td>
+        <td style='text-align: right'><input name='additionalflags' type='text' value='' /></td>
+        </tr>
+        </table>
         `
     });
 }
@@ -157,7 +175,10 @@ async function createCluster(previousData: any, context: Context): Promise<strin
         result: runnable
     };
 
-    context.minikube.start();
+    context.minikube.start({
+        vmDriver: previousData.vmdriver,
+        additionalFlags: previousData.additionalflags
+    } as MinikubeOptions);
 
     const title = createResult.result.succeeded ? 'Cluster creation has started' : `Error ${createResult.actionDescription}`;
     const message = succeeded(createResult.result) ?
