63ccc0eb73a3c3b96b2ccbbe28e8a10b4cefdbab
itowlson <github@hestia.cc>
2019-01-24 11:30:06 +1300

    Having no disabled linters crashed the linting system (#448)

--- src/components/config/config.ts
+++ src/components/config/config.ts
@@ -154,5 +154,5 @@ export function getDisableLint(): boolean {
 
 export function getDisabledLinters(): string[] {
     const config = vscode.workspace.getConfiguration(EXTENSION_CONFIG_KEY);
-    return config['disable-linters'] as string[];
+    return config['disable-linters'] as string[] || [];
 }
