5728c10cf969822e29eca1cdcefff103ae0a0831
Hanxiao Liu <hanli@microsoft.com>
2019-01-25 10:18:32 +0800

    Fix telemetry wording and update README

--- src/components/clusterprovider/azure/azureclusterprovider.ts
+++ src/components/clusterprovider/azure/azureclusterprovider.ts
@@ -217,7 +217,7 @@ async function createCluster(previousData: any, context: azure.Context): Promise
         } };
     const createResult = await azure.createCluster(context, options);
 
-    reporter.sendTelemetryEvent("createCluster", { result: createResult.result.succeeded ? "success" : "fail", clusterType: previousData.clusterType });
+    reporter.sendTelemetryEvent("clustercreation", { result: createResult.result.succeeded ? "success" : "failure", clusterType: previousData.clusterType });
 
     const title = createResult.result.succeeded ? 'Cluster creation has started' : `Error ${createResult.actionDescription}`;
     const additionalDiagnostic = diagnoseCreationError(createResult.result);
@@ -284,7 +284,7 @@ function renderConfigurationResult(configureResult: ActionResult<azure.Configure
     const configResult = result.result;
     const clusterServiceString = result.result.clusterType === "aks" ? "Azure Kubernetes Service" : "Azure Container Service";
 
-    reporter.sendTelemetryEvent("addCluster", { result: configureResult.result.succeeded ? "success" : "fail", clusterType: clusterServiceString });
+    reporter.sendTelemetryEvent("clusterregistration", { result: configureResult.result.succeeded ? "success" : "failure", clusterType: configResult.clusterType });
 
     const pathMessage = configResult.cliOnDefaultPath ? '' :
         '<p>This location is not on your system PATH. Add this directory to your path, or set the VS Code <b>vs-kubernetes.kubectl-path</b> config setting.</p>';
