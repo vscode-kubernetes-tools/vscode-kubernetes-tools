dc48cba01464dea3e39f71c1c29ad21d02b48468
Ivan Towlson <itowlson@microsoft.com>
2019-03-04 15:46:31 +1300

    Contract warnings

--- src/api/contract/command-targets/v1.ts
+++ src/api/contract/command-targets/v1.ts
@@ -1,3 +1,7 @@
+// This module is contractual and should not be changed after release.
+// It should be in sync with vscode-kubernetes-tools-api/ts/command-targets/v1.ts
+// at all times.
+
 export interface CommandTargetsV1 {
     resolve(target?: any): CommandTargetsV1.CommandTarget | undefined;
 }
--- src/api/contract/kubectl/v1.ts
+++ src/api/contract/kubectl/v1.ts
@@ -1,5 +1,5 @@
 // This module is contractual and should not be changed after release.
-// It should be in sync with vscode-kubernetes-tools-api/ts/clusterprovider/v1.ts
+// It should be in sync with vscode-kubernetes-tools-api/ts/kubectl/v1.ts
 // at all times.
 
 export interface KubectlV1 {
