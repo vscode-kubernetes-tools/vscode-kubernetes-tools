c8e541ad7b9cc53cb6199ebc7a79e4290135241f
Ivan Towlson <itowlson@microsoft.com>
2019-03-05 10:05:18 +1300

    Interim commit: the explorer hierarchy is a beast

--- src/api/contract/command-targets/v1.ts
+++ src/api/contract/command-targets/v1.ts
@@ -8,14 +8,58 @@ export interface CommandTargetsV1 {
 
 export namespace CommandTargetsV1 {
 
-    export interface KubernetesExplorerNode {
+    interface KubernetesExplorerNodeBase {
         readonly targetType: 'kubernetes-explorer-node';
-        readonly id: string;
     }
+
+    export interface KubernetesExplorerResourceNode extends KubernetesExplorerNodeBase {
+        readonly nodeType: 'resource';
+        readonly namespace: string | null;
+        readonly resourceId: string;
+        readonly metadata?: any;
+    }
+
+    export interface KubernetesExplorerGroupingFolderNode extends KubernetesExplorerNodeBase {
+        readonly nodeType: 'folder.grouping';
+    }
+
+    export interface KubernetesExplorerResourceFolderNode extends KubernetesExplorerNodeBase {
+        readonly nodeType: 'folder.resource';
+        readonly resourceKind: string;
+    }
+
+    export interface KubernetesExplorerContextNode extends KubernetesExplorerNodeBase {
+        readonly nodeType: 'context';
+    }
+
+    export interface KubernetesExplorerNamespaceNode extends KubernetesExplorerNodeBase {
+        readonly nodeType: 'namespace';
+    }
+
+    export interface KubernetesExplorerConfigDataItemNode extends KubernetesExplorerNodeBase {
+        readonly nodeType: 'configitem';
+    }
+
+    export interface KubernetesExplorerErrorNode extends KubernetesExplorerNodeBase {
+        readonly nodeType: 'error';
+    }
+
+    export interface KubernetesExplorerHelmReleaseNode extends KubernetesExplorerNodeBase {
+        readonly nodeType: 'helm.release';
+    }
+
+    export type KubernetesExplorerNode =
+        KubernetesExplorerResourceNode |
+        KubernetesExplorerGroupingFolderNode |
+        KubernetesExplorerResourceFolderNode |
+        KubernetesExplorerContextNode |
+        KubernetesExplorerNamespaceNode |
+        KubernetesExplorerErrorNode |
+        KubernetesExplorerHelmReleaseNode;
 
     export interface HelmExplorerNode {
         readonly targetType: 'helm-explorer-node';
     }
 
-    export type CommandTarget = KubernetesExplorerNode | HelmExplorerNode;
+    export type CommandTarget = KubernetesExplorerNodeBase | HelmExplorerNode;
 }
