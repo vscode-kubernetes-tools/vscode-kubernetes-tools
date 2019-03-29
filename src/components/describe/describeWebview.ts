523baba53c3268ff0b70def427b99022b6113109
Brendan Burns <bburns@microsoft.com>
2019-03-29 12:25:29 -0700

    Add a refresh button for 'describe' (#483)

--- src/components/describe/describeWebview.ts
+++ src/components/describe/describeWebview.ts
@@ -1,13 +1,22 @@
 import * as vscode from 'vscode';
+import { ShellResult } from '../../shell';
 import { WebPanel } from '../webpanel/webpanel';
 
 export class DescribePanel extends WebPanel {
     public static readonly viewType = 'vscodeKubernetesDescribe';
+    private static readonly describeContext = 'vscodeKubernetesDescribeContext';
+    private static activePanel: DescribePanel | undefined = undefined;
     public static currentPanels = new Map<string, DescribePanel>();
 
-    public static createOrShow(content: string, resource: string) {
+    public static refreshCommand() {
+        if (DescribePanel.activePanel) {
+            DescribePanel.activePanel.doRefresh();
+        }
+    }
+
+    public static createOrShow(content: string, resource: string, refresh: () => Promise<ShellResult | undefined>) {
         const fn = (panel: vscode.WebviewPanel, content: string, resource: string): DescribePanel => {
-            return new DescribePanel(panel, content, resource);
+            return new DescribePanel(panel, content, resource, refresh);
         };
         WebPanel.createOrShowInternal<DescribePanel>(content, resource, DescribePanel.viewType, "Kubernetes Describe", DescribePanel.currentPanels, fn);
     }
@@ -15,9 +24,46 @@ export class DescribePanel extends WebPanel {
     private constructor(
         panel: vscode.WebviewPanel,
         content: string,
-        resource: string
+        resource: string,
+        private readonly refresh: () => Promise<ShellResult | undefined>
     ) {
         super(panel, content, resource, DescribePanel.currentPanels);
+
+        const setActiveContext = (active: boolean) => {
+            vscode.commands.executeCommand('setContext', DescribePanel.describeContext, active);
+            DescribePanel.activePanel = active ? this : undefined;
+        };
+
+        this.panel.onDidChangeViewState((evt: vscode.WebviewPanelOnDidChangeViewStateEvent) => {
+            setActiveContext(evt.webviewPanel.active);
+        });
+        setActiveContext(true);
+        this.panel.webview.onDidReceiveMessage(
+            async (message) => {
+                switch (message.command) {
+                    case 'refresh':
+                        await this.doRefresh();
+                        break;
+                }
+            },
+            undefined
+        );
+    }
+
+    private async doRefresh() {
+        const result = await this.refresh();
+        if (!result) {
+            vscode.window.showErrorMessage('Error refreshing!');
+            return;
+        }
+        if (result.code !== 0) {
+            vscode.window.showErrorMessage(`Error refreshing: ${result.stderr}`);
+            return;
+        }
+        this.panel.webview.postMessage({
+            command: 'content',
+            content: result.stdout,
+        });
     }
 
     protected update() {
@@ -28,10 +74,22 @@ export class DescribePanel extends WebPanel {
     <head>
         <meta charset="UTF-8">
         <title>Kubernetes describe ${this.resource}</title>
+        <script>
+            const vscode = acquireVsCodeApi();
+
+            window.addEventListener('message', event => {
+                const message = event.data;
+                switch (message.command) {
+                    case 'content':
+                        const elt = document.getElementById('content');
+                        elt.innerText = message.content;
+                }
+            });
+        </script>
     </head>
     <body>
         <code>
-            <pre>${this.content}</pre>
+            <pre id='content'>${this.content}</pre>
         </code>
     </body>
     </html>`;
