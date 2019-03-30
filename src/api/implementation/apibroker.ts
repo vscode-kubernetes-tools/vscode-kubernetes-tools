b8a0925b5cf9836d9693eaf4127d07647342e314
itowlson <ivan.towlson@microsoft.com>
2019-03-29 18:04:24 -0700

    Added a basic Helm API

--- src/api/implementation/apibroker.ts
+++ src/api/implementation/apibroker.ts
@@ -2,6 +2,7 @@ import { APIBroker, API } from "../contract/api";
 import { versionUnknown } from "./apiutils";
 import * as clusterprovider from "./clusterprovider/versions";
 import * as kubectl from "./kubectl/versions";
+import * as helm from "./helm/versions";
 import * as clusterExplorer from "./cluster-explorer/versions";
 import { ClusterProviderRegistry } from "../../components/clusterprovider/clusterproviderregistry";
 import { Kubectl } from "../../kubectl";
@@ -13,6 +14,7 @@ export function apiBroker(clusterProviderRegistry: ClusterProviderRegistry, kube
             switch (component) {
                 case "clusterprovider": return clusterprovider.apiVersion(clusterProviderRegistry, version);
                 case "kubectl": return kubectl.apiVersion(kubectlImpl, version);
+                case "helm": return helm.apiVersion(version);
                 case "clusterexplorer": return clusterExplorer.apiVersion(explorer, version);
                 default: return versionUnknown;
             }
