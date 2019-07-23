1970c75fc383687111d44fd1fd068513f6431352
Ivan Towlson <itowlson@microsoft.com>
2019-07-23 13:48:24 +1200

    Faffing and fixing

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,9 @@ const webpack = require('webpack');
 
 /**@type {import('webpack').Configuration}*/
 const config = {
-  target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-
-  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  target: 'node',
+  entry: './src/extension.ts',
   output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'dist'),
     filename: 'extension.js',
     libraryTarget: 'commonjs2',
@@ -19,7 +17,7 @@ const config = {
   },
   devtool: 'source-map',
   externals: {
-    vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    vscode: 'commonjs vscode',
     bufferutil: 'commonjs bufferutil',
     '../build/Release/bufferutil': 'commonjs ../build/Release/bufferutil',
     '../build/default/bufferutil': 'commonjs ../build/default/bufferutil',
@@ -33,7 +31,6 @@ const config = {
       new webpack.IgnorePlugin(/^electron$/)
   ],
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
     extensions: ['.ts', '.js', '.json']
   },
   module: {
