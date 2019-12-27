a263744b7a91f972d6ec3c9e8b4d963bf82a6905
Luca Stocchi <lstocchi@redhat.com>
2019-12-27 13:14:34 +0100

    added automatic set new watcher if current fails for connreset error

--- src/components/kubectl/watch.ts
+++ src/components/kubectl/watch.ts
@@ -19,21 +19,34 @@ export class WatchManager {
     }
 
     public async addWatch(name: string, apiUri: string, params: any, callback: (phase: string, obj: any) => void): Promise<void> {
+        if (this.watchers.has(name)) {
+            return;
+        }
         const kc = await loadKubeconfig();
         const kcWatch = new kubernetes.Watch(kc);
-        const watcher = kcWatch.watch(apiUri,
-                                      params,
-                                      callback,
-                                      // done callback is called if the watch terminates normally
-                                      (err) => {
-                                          // tslint:disable-next-line:no-console
-                                          //err.code === "ECONNRESET";
-                                          console.log(err);
-                                      });
+        const doneCallback = (err: any) => {
+                                            // tslint:disable-next-line:no-console
+                                            // Error: read ECONNRESET
+                                            // at TLSWrap.onStreamRead (internal/stream_base_commons.js:183:27)
+                                            if (err &&
+                                                (err as Error).name === "ECONNRESET") {
+                                                    this.removeWatch(name);
+                                                    this.addWatch(name, apiUri, params, callback);
+                                            }
+                                            console.log(err);
+                                            };
+        const watcher: Request = kcWatch.watch(apiUri,
+                                               params,
+                                               callback,
+                                               doneCallback
+                                               );
         this.watchers.set(name, watcher);
     }
 
     public removeWatch(name: string) {
+        if (!this.watchers.has(name)) {
+            return;
+        }
         const watcher = this.watchers.get(name);
         if (watcher) {
             watcher.abort();
