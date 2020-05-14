69031338b0a3f676b7a489c97f450ffbda0f13a8
Ethan Lindemann-Michael <ethan.lindemann@outlook.com>
2020-05-14 00:32:40 -0700

    Add SelectPod command

--- src/extension.ts
+++ src/extension.ts
@@ -200,6 +200,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<APIBro
         registerCommand('extension.vsKubernetesDebugAttach', debugAttachKubernetes),
         registerCommand('extension.vsKubernetesDebugLocalTunnel', (target?: any) => { localTunnelDebugger.startLocalTunnelDebugSession(target); }),
         registerCommand('extension.vsKubernetesFindLocalTunnelDebugProviders', kubernetesFindLocalTunnelDebugProviders),
+        registerCommand('extension.vsKubernetesSelectPod', selectPodKubernetes),
         registerCommand('extension.vsKubernetesConfigureFromCluster', configureFromClusterKubernetes),
         registerCommand('extension.vsKubernetesCreateCluster', createClusterKubernetes),
         registerCommand('extension.vsKubernetesRefreshExplorer', () => treeProvider.refresh()),
@@ -1769,6 +1770,30 @@ const debugAttachKubernetes = async (explorerNode: ClusterExplorerResourceNode) 
     if (workspaceFolder) {
         new DebugSession(kubectl).attach(workspaceFolder, explorerNode ? explorerNode.name : undefined, explorerNode ? explorerNode.namespace || undefined : undefined);  // TODO: rationalise the nulls and undefineds
     }
+};
+
+const selectPodKubernetes = async () => {
+    const resource =  await kubectlUtils.getResourceAsJson<KubernetesCollection<Pod>>(kubectl, "pods");
+    if (!resource) {
+        return;
+    }
+
+    const podPickItems = resource.items.map((pod) => {
+        return {
+            label: `${pod.metadata.name} (${pod.spec.nodeName})`,
+            description: "pod",
+            name: pod.metadata.name,
+            namespace: pod.metadata.namespace,
+            containers: pod.spec.containers
+        };
+    });
+
+    const selectedPod = await vscode.window.showQuickPick(podPickItems, { placeHolder: `Select a pod` });
+    if (!selectedPod) {
+        return;
+    }
+
+    return selectedPod.name;
 };
 
 const debugInternal = (name: string, image: string) => {
