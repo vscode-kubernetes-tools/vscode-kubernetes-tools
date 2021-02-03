bf878b90bc8dcf4e074897db584a2a52f0b7bf50
itowlson <itowlson@microsoft.com>
2021-02-04 09:34:51 +1300

    Revert v1 and v1.1 APIs to their contracts (#877)

--- src/api/contract/cluster-explorer/v1_1.ts
+++ src/api/contract/cluster-explorer/v1_1.ts
@@ -76,11 +76,6 @@ export namespace ClusterExplorerV1_1 {
         readonly name: string;
     }
 
-    export interface ClusterExplorerHelmHistoryNode {
-        readonly nodeType: 'helm.history';
-        readonly name: string;
-    }
-
     export interface ClusterExplorerExtensionNode {
         readonly nodeType: 'extension';
     }
@@ -94,7 +89,6 @@ export namespace ClusterExplorerV1_1 {
         ClusterExplorerConfigDataItemNode |
         ClusterExplorerErrorNode |
         ClusterExplorerHelmReleaseNode |
-        ClusterExplorerHelmHistoryNode |
         ClusterExplorerExtensionNode;
 
     export interface ResourceKind {
