d2773d68d9515648fef2a32ea22c2252448b26c3
Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
2021-02-15 21:39:25 +0100

    Fix "Install Dependencies" being hardcoded to amd64 (#895)

--- src/components/kubectl/autoversion.ts
+++ src/components/kubectl/autoversion.ts
@@ -13,7 +13,7 @@ import { getKubeconfigPath } from '../kubectl/kubeconfig';
 import { getToolPath } from '../config/config';
 import { Host } from '../../host';
 import { mkdirpAsync } from '../../utils/mkdirp';
-import { platformUrlString, formatBin } from '../installer/installationlayout';
+import { platformUrlString, formatBin, platformArch } from '../installer/installationlayout';
 import * as installer from '../installer/installer';
 import { ExecResult } from '../../binutilplusplus';
 
@@ -76,8 +76,9 @@ async function downloadKubectlVersion(shell: Shell, host: Host, serverVersion: s
         return false;
     }
     const os = platformUrlString(shell.platform())!;
+    const arch = platformArch(os);
     const binFile = (shell.isUnix()) ? 'kubectl' : 'kubectl.exe';
-    const kubectlUrl = `https://storage.googleapis.com/kubernetes-release/release/${serverVersion}/bin/${os}/amd64/${binFile}`;
+    const kubectlUrl = `https://storage.googleapis.com/kubernetes-release/release/${serverVersion}/bin/${os}/${arch}/${binFile}`;
     // TODO: this feels a bit ugly and over-complicated - should perhaps be up to download.once to manage
     // showing the progress UI so we don't need all the operationKey mess
     const downloadResult = await host.longRunning({ title: `Downloading kubectl ${serverVersion}`, operationKey: binPath }, () =>
