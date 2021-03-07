9053b0c89ed335f0b9581d46ead870fcb774c1c9
Shreyas Karnik <karnik.shreyas@gmail.com>
2021-03-06 20:22:40 -0800

    adding support for Helm generate values.yaml

--- src/helm.exec.ts
+++ src/helm.exec.ts
@@ -231,7 +231,7 @@ export function helmInspectValues(arg: any) {
 export function helmGetValues(arg: any) {
     helmInspect(arg, {
         noTargetMessage:
-            "Helm Inspect Values is for packaged charts and directories. Launch the command from a file or directory in the file explorer. or a chart or version in the Helm Repos explorer.",
+            "Helm generate values.yaml is for packaged charts and directories. Launch the command from a file or directory in the file explorer. or a chart or version in the Helm Repos explorer.",
         inspectionScheme: helm.GET_VALUES_SCHEME,
         generateFile: true,
     });
