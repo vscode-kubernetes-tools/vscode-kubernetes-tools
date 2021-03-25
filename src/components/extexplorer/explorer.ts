f224d34901f88b2f7e7baf05a7203ee7c95d2daa
Yevhen Vydolob <yvydolob@redhat.com>
2021-03-25 17:06:58 +0200

    Recommend tags, instead of specific extensions
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

--- src/components/extexplorer/explorer.ts
+++ src/components/extexplorer/explorer.ts
@@ -4,86 +4,18 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { Kubectl } from '../../kubectl';
 
-const RECOMMENDED_EXT_CONTEXT = 'kubernetes:recommendedExtensions';
-
-enum VSCodeCommands {
-    SetContext = 'setContext',
-    Open = 'vscode.open'
-}
-
-interface RecommendedExtension {
-    id: string;
-    label: string;
-    description: string;
-    icon: string;
-}
 interface Recommendations {
-    crdApiToExtensions: { [crdApi: string]: string[] };
-    extensions: RecommendedExtension[];
+    [crdApi: string]: string[];
 }
 
-class ExtensionTreeItem extends vscode.TreeItem {
-
-    constructor(private ext: RecommendedExtension) {
-        super(ext.label, vscode.TreeItemCollapsibleState.None);
-    }
-
-    get description(): string {
-        return this.ext.description;
-    }
-
-    get iconPath(): vscode.Uri {
-        return vscode.Uri.parse(this.ext.icon);
-    }
-
-    get command(): vscode.Command {
-        return { command: VSCodeCommands.Open, arguments: [vscode.Uri.parse(`vscode:extension/${this.ext.id}`)] } as vscode.Command;
-    }
-
-}
-
-class RecommendedExtensionsView implements vscode.TreeDataProvider<RecommendedExtension>{
-
-    private extensions: RecommendedExtension[] | undefined;
-    private treeView: vscode.TreeView<RecommendedExtension>;
-    private onDidChangeTreeDataEmitter = new vscode.EventEmitter<RecommendedExtension | undefined>();
-    readonly onDidChangeTreeData: vscode.Event<RecommendedExtension | undefined> = this.onDidChangeTreeDataEmitter.event;
-
-    constructor() {
-        this.treeView = vscode.window.createTreeView('kubernetes.RecommendedExtensions', { treeDataProvider: this });
-    }
-
-    dispose(): void {
-        this.treeView.dispose();
-    }
-
-    getTreeItem(element: RecommendedExtension): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        return new ExtensionTreeItem(element);
-    }
-    getChildren(element?: RecommendedExtension): vscode.ProviderResult<RecommendedExtension[]> {
-        if (!element && this.extensions) {
-            return this.extensions;
+async function recommendExtensionsToUser(tags: string[]): Promise<void> {
+    if (tags.length > 0) { // do not prompt if we cannot recommend anything
+        const answer = await vscode.window.showInformationMessage('Do you want to view recommended extensions for your Kubernetes cluster?', 'Ignore', 'Show Recommendation');
+        if (answer === 'Show Recommendation') {
+            const searchValue = tags.map((it) => `@tag:${it}`).join(' ');
+            vscode.commands.executeCommand('workbench.extensions.search', searchValue);
         }
-        return undefined;
     }
-    getParent?(): vscode.ProviderResult<RecommendedExtension> {
-        return undefined; // we have plain list, so no parents
-    }
-
-    setExtensions(extensions: RecommendedExtension[]): void {
-        this.extensions = extensions;
-    }
-
-}
-
-async function recommendExtensionsToUser(extensions: RecommendedExtension[], subscription: vscode.Disposable[]): Promise<void> {
-    if (extensions.length > 0) { // do not prompt if we cannot recommend anything
-        const treeView = new RecommendedExtensionsView();
-        subscription.push(treeView);
-        treeView.setExtensions(extensions);
-        vscode.commands.executeCommand(VSCodeCommands.SetContext, RECOMMENDED_EXT_CONTEXT, true);
-    }
-
 }
 
 async function getCRDApiGroups(kubectl: Kubectl): Promise<string[]> {
@@ -117,17 +49,18 @@ async function readRecommendation(extPath: string): Promise<Recommendations> {
 }
 
 export async function recommendExtensions(kubectl: Kubectl, context: vscode.ExtensionContext): Promise<void> {
-    vscode.commands.executeCommand(VSCodeCommands.SetContext, RECOMMENDED_EXT_CONTEXT, false);
+    if (vscode.workspace.getConfiguration('extensions').get('ignoreRecommendations')) {
+        return;
+    }
     const crdApiGroups = await getCRDApiGroups(kubectl);
 
     const extensionToPromote = new Set<string>();
     const recommendation = await readRecommendation(context.extensionPath);
-    const recommendationsMap = recommendation.crdApiToExtensions;
     // collect extension id to promote based on installed operators
     for (const crdApi of crdApiGroups) {
-        for (const key in recommendationsMap) {
-            if (recommendationsMap.hasOwnProperty(key)) {
-                const value = recommendationsMap[key];
+        for (const key in recommendation) {
+            if (recommendation.hasOwnProperty(key)) {
+                const value = recommendation[key];
                 if (crdApi.startsWith(key)) {
                     value.forEach((val: string) => extensionToPromote.add(val));
                 }
@@ -135,22 +68,6 @@ export async function recommendExtensions(kubectl: Kubectl, context: vscode.Exte
         }
     }
 
-    // remove already installed extensions
-    if (extensionToPromote.size > 0) {
-        for (const ext of vscode.extensions.all) {
-            if (extensionToPromote.has(ext.id)) {
-                extensionToPromote.delete(ext.id);
-            }
-        }
-    }
 
-    // get needed extensions description from json
-    const extDescription: RecommendedExtension[] = [];
-    recommendation.extensions.forEach((ext) => {
-        if (extensionToPromote.has(ext.id)) {
-            extDescription.push(ext);
-        }
-    });
-
-    recommendExtensionsToUser(extDescription, context.subscriptions);
+    recommendExtensionsToUser(Array.from(extensionToPromote.values()));
 }
