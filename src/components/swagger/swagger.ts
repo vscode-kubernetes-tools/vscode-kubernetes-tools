336a9ec500a31781f3c02a152c8ef85377d03cec
Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
2021-07-06 05:54:10 +0200

    Fetch custom CRD schemas from active cluster (#924)

--- src/components/swagger/swagger.ts
+++ src/components/swagger/swagger.ts
@@ -3,6 +3,8 @@ import { Errorable, failed } from "../../errorable";
 import { Kubectl } from "../../kubectl";
 import { proxy } from "../kubectl/proxy";
 import { fs } from '../../fs';
+import * as kubectlUtils from '../../kubectlUtils';
+import { ExecResult } from '../../binutilplusplus';
 
 export type Swagger = any;
 
@@ -33,4 +35,36 @@ export async function getClusterSwagger(kubectl: Kubectl): Promise<Errorable<Swa
     } finally {
         proxySession.dispose();
     }
+}
+
+export async function getCrdSchemas(kubectl: Kubectl): Promise<{[key: string]: object | undefined} | undefined> {
+    const er = await kubectl.invokeCommand(`get crd -o jsonpath="{range .items[*].spec}[{.group}, {.names.plural}, {.names.singular}, {range .versions[?(.schema.openAPIV3Schema.properties)]}{.name}{\\" \\"}{end}]{end}"`);
+
+    if (ExecResult.failed(er)) {
+        kubectl.reportFailure(er, { whatFailed: 'Failed to get crds' });
+        return;
+    }
+    try {
+        const crdSchemasMapping: { [key: string]: object } = {};
+        const crdInfos = er.stdout.substring(1).split('[');
+        for (const crdInfo of crdInfos) {
+            const crdInfoAsArray = crdInfo.split(',');
+            const group = crdInfoAsArray[0].trim();
+            const plural = crdInfoAsArray[1].trim();
+            const singular = crdInfoAsArray[2].trim();
+            const versions = crdInfoAsArray[3].trim() !== '' ? crdInfoAsArray[3].trim().split(' ') : [];
+            if (versions.length > 0) {
+                const crd =  await kubectlUtils.getResourceAsJson<any>(kubectl, `crd ${plural}.${group}`, '', true);
+                if (!crd) {
+                    continue;
+                }
+                for (const version of versions) {
+                    const versionObject = crd.spec.versions ? crd.spec.versions.filter((v: { name: string }) => v.name === version) : [];
+                    crdSchemasMapping[`${group}/${version}@${singular}`] = versionObject.length > 0 ? versionObject[0].schema.openAPIV3Schema.properties : undefined;
+                }
+            }
+        }
+        return crdSchemasMapping;
+    } catch (e) { }
+    return;
 }
