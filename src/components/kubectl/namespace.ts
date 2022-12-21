cd3e89d2f3d594e5c14c31dc87e9f6e1c457bdfa
Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
2022-12-21 09:56:41 +0100

    extend minimal workflow to other commands (get, expose, scale, delete .. ) (#1006)
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- src/components/kubectl/namespace.ts
+++ src/components/kubectl/namespace.ts
@@ -6,6 +6,7 @@ import * as kuberesources from '../../kuberesources';
 import { Kubectl } from '../../kubectl';
 import { ClusterExplorerNode } from '../clusterexplorer/node';
 import { NODE_TYPES } from '../clusterexplorer/explorer';
+import * as config from '../config/config';
 
 
 export async function useNamespaceKubernetes(kubectl: Kubectl, explorerNode: ClusterExplorerNode) {
@@ -17,6 +18,7 @@ export async function useNamespaceKubernetes(kubectl: Kubectl, explorerNode: Clu
         }
     }
 
+    const isMinimalWorkflow = config.isMinimalWorkflow();
     const currentNS = await kubectlUtils.currentNamespace(kubectl);
     const kindName = await promptKindName(
         [kuberesources.allKinds.namespace],
@@ -24,7 +26,8 @@ export async function useNamespaceKubernetes(kubectl: Kubectl, explorerNode: Clu
         {
             prompt: 'What namespace do you want to use?',
             placeHolder: 'Enter the namespace to switch to or press enter to select from available list',
-            filterNames: [currentNS]
+            filterNames: [currentNS],
+            skipFreeTextPrompt: isMinimalWorkflow
         }
     );
 
