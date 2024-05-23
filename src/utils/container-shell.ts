57e9783fde4dcfaf540fe88475b81abc19bda3cf
ckung01 <chris.kung@yahooinc.com>
2024-05-24 02:30:20 +0800

    Added windows container shell support
    Signed-off-by: ckung01 <chris.kung@yahooinc.com>

--- src/utils/container-shell.ts
+++ src/utils/container-shell.ts
@@ -1,16 +1,29 @@
 import { Kubectl } from "../kubectl";
 import { ExecResult } from "../binutilplusplus";
 
-async function isBashOnContainer(kubectl: Kubectl, podName: string, podNamespace: string | undefined, containerName: string | undefined): Promise<boolean> {
+async function isFileOnContainer(kubectl: Kubectl, podName: string, podNamespace: string | undefined, containerName: string | undefined, checkCommand: string): Promise<boolean> {
     const nsarg = podNamespace ? `--namespace ${podNamespace}` : '';
     const containerCommand = containerName ? `-c ${containerName}` : '';
-    const result = await kubectl.invokeCommand(`exec ${podName} ${nsarg} ${containerCommand} -- ls -la /bin/bash`);
+    const result = await kubectl.invokeCommand(`exec ${podName} ${nsarg} ${containerCommand} -- ${checkCommand}`);
     return ExecResult.succeeded(result);
+}
+
+async function isBashOnContainer(kubectl: Kubectl, podName: string, podNamespace: string | undefined, containerName: string | undefined): Promise<boolean> {
+    const checkCommand = 'ls -la /bin/bash';
+    return isFileOnContainer(kubectl, podName, podNamespace, containerName, checkCommand);
+}
+
+async function isPowerShellOnContainer(kubectl: Kubectl, podName: string, podNamespace: string | undefined, containerName: string | undefined): Promise<boolean> {
+    const checkCommand = 'cmd /C where powershell';
+    return isFileOnContainer(kubectl, podName, podNamespace, containerName, checkCommand);
 }
 
 export async function suggestedShellForContainer(kubectl: Kubectl, podName: string, podNamespace: string | undefined, containerName: string | undefined): Promise<string> {
     if (await isBashOnContainer(kubectl, podName, podNamespace, containerName)) {
         return 'bash';
+    }
+    if (await isPowerShellOnContainer(kubectl, podName, podNamespace, containerName)) {
+        return 'powershell.exe';
     }
     return 'sh';
 }
