7f4ec08dedc7d349631a353008fb731a18ba5c8f
Tatsat Mishra <tatsat.mishra@microsoft.com>
2024-10-13 15:40:57 +1300

    SImple clipboard implementation.
    Signed-off-by: Tatsat Mishra <tatsat.mishra@microsoft.com>

--- src/components/platform/clipboard.ts
+++ src/components/platform/clipboard.ts
@@ -1,11 +1,12 @@
-import clipboard from 'clipboardy';
 import * as vscode from 'vscode';
 
-export async function write(text: string) {
-    const cb = (<any>vscode.env).clipboard;
-    if (cb) {
-        await cb.writeText(text);
-    } else {
-        await clipboard.write(text);
-    }
+export function copyTextToClipboard(text: string): void {
+    vscode.env.clipboard.writeText(text).then(
+        () => {
+            vscode.window.showInformationMessage('Text copied to clipboard!');
+        },
+        (err) => {
+            vscode.window.showErrorMessage('Failed to copy text: ' + err);
+        }
+    );
 }
