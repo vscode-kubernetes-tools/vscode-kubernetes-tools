264eaea4605cc00f0ae99b3b0e4f27b16a90492b
Tatsat Mishra <tatsat.mishra@microsoft.com>
2024-10-17 08:13:16 +1300

    Recommended cahnges: Add text explicit info.
    Signed-off-by: Tatsat Mishra <tatsat.mishra@microsoft.com>

--- src/components/platform/clipboard.ts
+++ src/components/platform/clipboard.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 export function copyTextToClipboard(text: string): void {
     vscode.env.clipboard.writeText(text).then(
         () => {
-            vscode.window.showInformationMessage('Text copied to clipboard!');
+            vscode.window.showInformationMessage(`${text} copied to clipboard!`);
         },
         (err) => {
             vscode.window.showErrorMessage('Failed to copy text: ' + err);
