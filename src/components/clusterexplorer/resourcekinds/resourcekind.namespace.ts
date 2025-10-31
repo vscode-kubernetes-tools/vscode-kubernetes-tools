ad0300bb536b50ff69f7cff66305935daae70f88
tejhan-diallo <tejhan.diallo@gmail.com>
2025-10-31 15:50:08 -0400

    Adding filtering for accessible namespaces
    Signed-off-by: tejhan-diallo <tejhan.diallo@gmail.com>

--- src/components/clusterexplorer/resourcekinds/resourcekind.namespace.ts
+++ src/components/clusterexplorer/resourcekinds/resourcekind.namespace.ts
@@ -5,6 +5,7 @@ import { ResourceKind } from '../../../kuberesources';
 import { Kubectl } from '../../../kubectl';
 import * as kubectlUtils from '../../../kubectlUtils';
 import { ClusterExplorerNode } from '../node';
+import { filterAccessibleNamespaces } from '../filter-accessible-namespaces';
 
 export const namespaceUICustomiser = {
     customiseTreeItem(resource: ResourceNode, treeItem: vscode.TreeItem): void {
@@ -21,6 +22,22 @@ export const namespaceUICustomiser = {
 export const namespaceLister = {
     async list(kubectl: Kubectl, kind: ResourceKind): Promise<ClusterExplorerNode[]> {
         const namespaces = await kubectlUtils.getNamespaces(kubectl);
-        return namespaces.map((ns) => ResourceNode.create(kind, ns.name, ns.metadata, { namespaceInfo: ns }));
+        
+        const config = vscode.workspace.getConfiguration('vs-kubernetes');
+        const hideInaccessible = config['vs-kubernetes.hideInaccessibleNamespaces'] || false;
+        
+        if (!hideInaccessible) {
+            // default behavior
+            return namespaces.map((ns) => ResourceNode.create(kind, ns.name, ns.metadata, { namespaceInfo: ns }));
+        }
+        
+        // filter to only accessible namespaces
+        const namespaceNames = namespaces.map(ns => ns.name);
+        const accessibleNames = await filterAccessibleNamespaces(kubectl, namespaceNames);
+        const accessibleSet = new Set(accessibleNames);
+        
+        return namespaces
+            .filter(ns => accessibleSet.has(ns.name))
+            .map((ns) => ResourceNode.create(kind, ns.name, ns.metadata, { namespaceInfo: ns }));
     }
 };
